065176e146d6098d69cff0c4699f05a8e625bed9
Viwu <sinconsistencia@gmail.com>
2025-10-08 06:49:23 -0500

    InitState + tima folder

---
+++ source/sphis/tima/InitState.hx
@@ -0,0 +1,16 @@
+package sphis.tima;
+
+import flixel.FlxState;
+
+class InitState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
--- source/sphis/tima/play/PlayState.hx
+++ source/sphis/tima/play/PlayState.hx
@@ -1,4 +1,4 @@
-package;
+package sphis.tima.play;
 
 import flixel.FlxState;
 
