d1df5056629e063035dda65e1715a68a8c0d8ad6
Viwu <sinconsistencia@gmail.com>
2025-10-08 07:31:37 -0500

    jkukuiuihbuihuihu

--- source/sphis/tima/backend/systems/day/DayDataHandler.hx
+++ source/sphis/tima/backend/systems/day/DayDataHandler.hx
@@ -7,26 +7,26 @@ class DayDataHandler
 	public static var dayColorsFilePath = 'assets/data/systems/day-colors.json';
 	public static var dayTicksFilePath = 'assets/data/systems/day-ticks.json';
 
-	macro public static function getDayColors():DayColors
+	public static function getDayColors():DayColors
 	{
 		trace('Getting day-colors data...');
 
 		return null;
 	}
 
-	macro public static function getDayTicks():DayTicks
+	public static function getDayTicks():DayTicks
 	{
 		trace('Getting day-ticks data...');
 
 		return null;
 	}
 
-	macro public static function getDayValues():Array<MergedDayField>
+	public static function getDayValues():Array<MergedDayField>
 	{
 		var dayValues:Array<MergedDayField> = [];
 
 		var dayColors:DayColors = getDayColors();
-		var dayTicks:DayTick = getDayTicks();
+		var dayTicks:DayTicks = getDayTicks();
 
 		trace('Getting dayValues data...');
 
