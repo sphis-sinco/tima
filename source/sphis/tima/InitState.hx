a89104c8300a21a25bff5a0933fa618be744413e
Viwu <sinconsistencia@gmail.com>
2025-10-08 06:49:43 -0500

    switch to playstate

--- source/sphis/tima/InitState.hx
+++ source/sphis/tima/InitState.hx
@@ -1,12 +1,15 @@
 package sphis.tima;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import sphis.tima.play.PlayState;
 
 class InitState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+		FlxG.switchState(() -> new PlayState());
 	}
 
 	override public function update(elapsed:Float)
